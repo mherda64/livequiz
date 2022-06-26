6ede9504c81758e5aa8dfe67078252323efe6a4d
mherda64 <mherda64@gmail.com>
2022-06-27 00:05:29 +0200

    New questions, chart colours

--- livequiz_server/src/main/java/com/mherda/livequiz/LiveQuizApplication.java
+++ livequiz_server/src/main/java/com/mherda/livequiz/LiveQuizApplication.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -38,12 +39,25 @@ class MyRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var first = generateAndPersistQuestion("Pytanie pierwsze");
-        generateAndPersistQuestion("Pytanie drugie");
-        generateAndPersistQuestion("Pytanie trzecie");
+        var first = generateAndPersistQuestion("Recordy w Javie to:", List.of(
+                Pair.of("Typy prymitywne", false),
+                Pair.of("Klasy", true),
+                Pair.of("Typy wyliczeniowe", false),
+                Pair.of("Żadne z powyższych", false)
+        ));
+        var second = generateAndPersistQuestion("Project Valhalla zakłada dodanie do Javy:", List.of(
+                Pair.of("Typów prymitywnych deifniowanych przez programistę", true),
+                Pair.of("Automatyczne generowanie geterów i seterów", false),
+                Pair.of("Kompilację kodu bajtowego Javy do kodu natywnego", false)
+        ));
+        var third = generateAndPersistQuestion("Project Loom zakłada dodanie do Javy:", List.of(
+                Pair.of("Ulepszoną wydajność pracy wątków systemowych", false),
+                Pair.of("Wirtualnych wątków", true),
+                Pair.of("Nowego API do wykonywania kodu natywnego", false)
+        ));
 
         var votingSession = VotingSession.builder()
-                .sessionState(SessionState.OPEN)
+                .sessionState(SessionState.FINISHED_CLOSED)
                 .startDate(LocalDateTime.now())
                 .question(first)
                 .votes(new ArrayList<>())
@@ -53,6 +67,14 @@ class MyRunner implements CommandLineRunner {
 
     private Question generateAndPersistQuestion(String content) {
         var question = generateQuestion(content);
+        questionRepository.save(question);
+        question.getAvailableAnswers().forEach(answer -> answer.setQuestion(question));
+        answerRepository.saveAll(question.getAvailableAnswers());
+        return question;
+    }
+
+    private Question generateAndPersistQuestion(String content, List<Pair<String, Boolean>> answers) {
+        var question = generateQuestion(content, answers);
         questionRepository.save(question);
         question.getAvailableAnswers().forEach(answer -> answer.setQuestion(question));
         answerRepository.saveAll(question.getAvailableAnswers());
@@ -69,6 +91,17 @@ class MyRunner implements CommandLineRunner {
         return Question.builder()
                 .content(content)
                 .availableAnswers(answers)
+                .build();
+    }
+
+    private Question generateQuestion(String content, List<Pair<String, Boolean>> answers) {
+        var resultAnswers = answers.stream().map(
+                a -> Answer.builder().content(a.getFirst()).isCorrect(a.getSecond()).build()
+        ).toList();
+
+        return Question.builder()
+                .content(content)
+                .availableAnswers(resultAnswers)
                 .build();
     }
 }
